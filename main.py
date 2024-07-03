daf43ab6ea9cd52698620c9a60743a362416a729
Kizovani <perailaki@gmail.com>
2024-07-03 01:51:11 -0400

    20-20-20 timer fully working, ipconfig stats simplified since not all info can be displayed on windows notifications since they are limited to 4 lines anyway.

--- main.py
+++ main.py
@@ -1,28 +1,60 @@
-# TODO: Basic IPConfig working, but I want to also add public ip adress and ping time to 8.8.8.8, which probably should
-# start timing itself before everything else to allow for max time, and possibly also wait for it to finish if needed
+# TODO: Basic IPConfig working, remove old code since cant be used in under 4 lines for windows notification
 
 # TODO: add a regular click mode, and add some sort of setting for "on regular click, do foo", that way user will not
-# need to right click then left click for what they want if its just one thing
+# need to right click then left click for what they want if its just one thing, this doesnt seem that useful tbh
 
 # TODO: add internet check incase no internet to get requests to display "connection error" or something
+# THIS IS ONLY NEEDED FOR QUOTES SO FAR
 
 # TODO: battery info/screenshot app/implement translucent tb??/panda that follows cursor around?/ask gpt for more ideas
+# translucent is so weird, maybe I could port it in? But idk I dont think its open source, we can check later
+# maybe my own script to change the system sounds levels
 
 # TODO: I want to make all these "modules" (weather, battery, screenshot etc) toggleable, and possibly a custom
 # installer for the ones you want, but I think thats gonna be overkill
 
+import threading
+import time
 import pystray
 import PIL.Image
 import requests
-import threading  # dont think ill need this for now, but should look into using it when app gets big
 import psutil
 import socket
 import ping3
-import time
-
-# for info on api for quotes: https://forismatic.com/en/api/
 
 image = PIL.Image.open("bamboo-canes.png")
+
+# Global timing variables
+timer_running = False
+timer_thread = None
+
+
+def twenty_twenty_twenty_timer():
+    global timer_running, timer_thread
+    if timer_running:
+        icon.notify("Time for a 20-20-20 break!", "TrayPanda")
+        print("Time for a 20-20-20 break!")
+        # Schedule the next break after 20 minutes
+        timer_thread = threading.Timer(20 * 60, twenty_twenty_twenty_timer)
+        timer_thread.start()
+
+
+def toggle_timer(icon, item):
+    global timer_running, timer_thread
+    if not timer_running:
+        print("Timer started.")
+        icon.notify("20-20-20 timer started", "TrayPanda")
+        timer_running = True
+        # Start the timer initially after 20 minutes
+        timer_thread = threading.Timer(20 * 60, twenty_twenty_twenty_timer)
+        timer_thread.start()
+    else:
+        print("Timer stopped.")
+        icon.notify("20-20-20 timer stopped", "TrayPanda")
+        timer_running = False
+        # Cancel any running timer thread
+        if timer_thread:
+            timer_thread.cancel()
 
 
 def get_quote():
@@ -39,15 +71,11 @@ def get_quote():
         return f"Error: {e}"
 
 
-# Max notification size in windows is limited to 4 lines, will probably make a separate page for this, also, default
-# gateway is not working lol
 def get_ipconfig_info():
     try:
         info = psutil.net_if_addrs()
         stats = psutil.net_if_stats()
-        ping_time = ping3.ping('8.8.8.8')
 
-        # Filter out the loopback and non-active interfaces
         active_interfaces = [name for name, stat in stats.items() if
                              stat.isup and name != 'Loopback Pseudo-Interface 1']
 
@@ -56,52 +84,30 @@ def get_ipconfig_info():
             ipv4 = next((addr.address for addr in info[main_adapter] if addr.family == socket.AF_INET), 'N/A')
             ipv6 = next((addr.address for addr in info[main_adapter] if addr.family == socket.AF_INET6), 'N/A')
             mac = next((addr.address for addr in info[main_adapter] if addr.family == psutil.AF_LINK), 'N/A')
-            netmask = next((addr.netmask for addr in info[main_adapter] if addr.family == socket.AF_INET), 'N/A')
-            broadcast = next((addr.broadcast for addr in info[main_adapter] if addr.family == socket.AF_INET), 'N/A')
-            gateway = 'N/A'  # Default gateway logic can be added here if necessary
 
             return (f"Adapter: {main_adapter}\n"
                     f"IPv4: {ipv4}\n"
                     f"IPv6: {ipv6}\n"
-                    f"MAC: {mac}\n"
-                    f"Netmask: {netmask}\n"
-                    f"Broadcast: {broadcast}\n"
-                    f"Default Gateway: {gateway}\n"
-                    f"Ping Time (to 8.8.8.8): {ping_time * 1000:.2f} ms")
+                    f"MAC: {mac}\n")
         else:
             return "No active network adapters found."
     except Exception as e:
         return f"Error: {e}"
 
 
-def twenty_twenty_twenty_timer():
-    while True:
-        time.sleep(20*60)
-        icon.notify("20-20-20 Rule!", "TrayPanda")
-        time.sleep(25)  # added five seconds to account for reading the notification
-
-
-def start_timer():
-    timer_thread = threading.Thread(target=twenty_twenty_twenty_timer())
-    timer_thread.daemon = True  # Terminate the thread when main program stops
-    timer_thread.start()
-
-
-# icon is the tray icon and item what is clicked
 def on_clicked(icon, item):
     if item.text == "Random Quote":
         icon.notify(get_quote(), "Quote of the day!")
     elif item.text == "IPConfig Info":
         icon.notify(get_ipconfig_info(), "IPConfig Info")
-    elif item.text == "Start 20-20-20 Timer":
-        start_timer()
+    elif item.text == "Toggle Timer":
+        toggle_timer(icon, item)
 
 
 icon = pystray.Icon("Bamboo", image, menu=pystray.Menu(
     pystray.MenuItem("Random Quote", on_clicked),
     pystray.MenuItem("IPConfig Info", on_clicked),
-    # Add a new menu item for starting the timer
-    pystray.MenuItem("Start 20-20-20 Timer", on_clicked)
+    pystray.MenuItem("Toggle Timer", on_clicked)
 ), HAS_NOTIFICATION=True)
 
 icon.run()
